06189ffd6daec825bba971edb4ac0553645cf519
Williames Camilo <williamescw@gmail.com>
2024-08-02 16:59:29 -0300

    Alterações, Base.html, Configuração Adicionar Prodroudto, Bootstrap - P57/85

--- estoque/views.py
+++ estoque/views.py
@@ -1,21 +1,21 @@
+from django.http import HttpResponse
 from django.shortcuts import redirect, render
 
 from .forms import ProdutoForm
 from .models import Produto
 
-# Create your views here.
-# estoque/views.py
 
 def adicionar_produto(request):
     if request.method == 'POST':
         form = ProdutoForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('nome_da_url_para_lista_de_produtos')  # Substitua pelo nome da URL de destino
+            # Redireciona para a view 'home' após adicionar o produto
+            return redirect('home')
     else:
         form = ProdutoForm()
-    
     return render(request, 'estoque/adicionar_produto.html', {'form': form})
 
 def home(request):
-    return render(request, 'estoque/home.html')
+    produtos = Produto.objects.all()  # Busca todos os produtos cadastrados
+    return render(request, 'estoque/home.html', {'produtos': produtos})
